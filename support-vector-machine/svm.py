97f971096b880f4d47eb6f20e030e15214f29840
Franziska Schloesser <schloesser@zib.de>
2018-03-03 23:05:16 +0100

    modify python file

--- support-vector-machine/svm.py
+++ support-vector-machine/svm.py
@@ -2,41 +2,58 @@
 from pyscipopt import Model, quicksum
 from data.load_cancer import load_cancer
 import numpy as np
+import random
 
 
 #############    Get user input
 #############################################################################
 
-mode = "linear" # mode is in ['linear', 'sparse']
+mode = "sparse" # mode is in ['linear', 'sparse']
 
 # sparsity of classifier: percentage of present features not being used for classification
-sparsity = .2  # between 0.0 and 1.0 (1: all features are used, 0: none are used)
+sparsity = .5  # between 0.0 and 1.0 (1: all features are used, 0: none are used)
 
 # C is a regularization parameter
-C = .125 # positive float
+C = 10.0 # positive float
 
 # set the bounds for the weights. Weights will be between [-weightBound, weightBound]
-weightBound = 10.0 # positive float
-classWeights= [1.0, 1.0] # list of length 2
+weightBound = 50.0 # positive float
+# set these to [1.0, 1.0] if you don't want to manually balance anything
+balance = [1.0, 1.0] # list of length 2
 
 # set the time limit
-tLim = 5.0 # positive float
-# set the verbosity level
-verbosity = 0 # 0: no verbosity, 1: verbose
+tLim = 10.0 # positive float
+
+# train on how many samples?
+n_train = 150
 
 #############    Load data
 #############################################################################
 
 # get data
 dataset = load_cancer()
-X = np.array(dataset.data)
-Y = np.array(dataset.targets)
+X_orig = np.array(dataset.data)
+Y_orig = np.array(dataset.targets)
 
 #############    Initialize variables
 #############################################################################
 
-nfeatures = len(X[0]) # number of features (dimension of space)
-nexamples = len(Y) # number of examples
+nfeatures = len(X_orig[0]) # number of features (dimension of space)
+n = len(Y_orig)
+
+randnums = random.sample(range(n), n_train)
+
+# save the first n datapoints for prediction
+X_predict = X_orig[randnums]
+Y_predict = Y_orig[randnums]
+n = len(Y_predict)
+
+diff = [n for n in range(n) if not n in randnums]
+
+X = X_orig[diff]
+Y = Y_orig[diff]
+
+nexamples = len(Y) # number of training examples
 
 #############    Model
 #############################################################################
@@ -45,18 +62,16 @@ nexamples = len(Y) # number of examples
 model = Model("SCIP-SVM")
 # set timelimit
 model.setRealParam("limits/time", tLim)
-# set verbosity
-model.setIntParam("display/verblevel", verbosity)
 
 #############    Add problem variables
 #############################################################################
 
 omegas = []
-# add feature weight variables
+# add weight variables and offset variable (as last one)
 for f in range(nfeatures + 1):
     omegas.append(model.addVar(vtype='C',
                                name="omega_%d" % f,
-                               ub=weightBound,
+                               ub = weightBound,
                                lb = -weightBound))
 
 xis = []
@@ -73,13 +88,13 @@ objvar = model.addVar(vtype='C',
                       name="objective",
                       obj=1.0)
 
-# apply correction to account for unequal proportions of positive and negative example
-objCoeffs = [classWeights[0 if yi == -1 else 1] for yi in Y]
+# apply correction to account for unequal proportions of positive and negative examples
+balance_coeffs = [balance[0 if yi == -1 else 1] for yi in Y]
 
 model.addCons(quicksum(.5 *omegas[f]*omegas[f] for f in range(nfeatures)) \
-                   + quicksum(C / float(nexamples) * objCoeffs[x] * xis[x] for x in range(nexamples)) - objvar <= 0,
+                   + quicksum(C / float(nexamples) * balance_coeffs[x] * xis[x] for x in range(nexamples))
+                   - objvar <= 0,
               name="objective_function")
-
 
 #############    Add model formulation
 #############################################################################
@@ -114,13 +129,40 @@ if not sols:
 
 weights = [model.getSolVal(sols[0], omegas[f]) for f in range(nfeatures)]
 offset = model.getSolVal(sols[0], omegas[nfeatures])
+primalbound = model.getPrimalbound()
 
 #############    Predict
 #############################################################################
-# predict
-result = list(map(
-        lambda X:
-            offset + sum((weights[f] * X[f] for f in range(len(X)))),
-        X))
 
-print(result)
+pairs = np.array(list(zip(X_predict, Y_predict)))
+signed_penalties = list(map(
+        lambda x:
+            (x[1]*max(0, 1 - (offset + sum(weights[f] * x[0][f] for f in range(len(x[0]))))*x[1])),
+        pairs))
+
+
+n_wrong_class = sum([0 if p==0 else 1 for p in signed_penalties])
+n_false_positives = sum([1 if p<0 else 0 for p in signed_penalties])
+n_false_negatives = sum([1 if p>0 else 0 for p in signed_penalties])
+
+print("")
+print("RESULTS:")
+print("")
+print("mode = {}".format(mode))
+print("sparsity = {}".format(sparsity))
+print("C = {}".format(C))
+print("weightBound = {}".format(weightBound))
+print("balance = {}".format(balance))
+print("tlim = {}".format(tLim))
+print("")
+print("Trained on {} datapoints".format(len(Y)))
+print("Predicted on {} datapoints".format(len(Y_predict)))
+print("")
+print("W = {}".format(weights))
+print("b = {}".format(offset))
+print("obj = {}".format(primalbound))
+print("")
+print("Number of missclassifications: {} out of {} ({} %)".format(n_wrong_class, n, 100*n_wrong_class/n))
+print("Number of false positives: {} out of {} ({} %)".format(n_false_positives, n, 100*n_false_positives/n))
+print("Number of false negatives: {} out of {} ({} %)".format(n_false_negatives, n, 100*n_false_negatives/n))
+
